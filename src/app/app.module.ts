7bc69293a29c30bf7ec4ad36999f4bc46d59c001
WooJonnyBoy <elitle111.kha@gmail.com>
2024-01-30 19:44:25 +0200

    fix: some fixes

--- src/app/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-@NgModule({
-  imports: [BrowserModule, HttpClientModule],
-})
-export class AppModule {}
